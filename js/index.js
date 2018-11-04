2b9cb3186bdaadfec6d6684f1502bda89b587ac9
Bartixxx <mcbplay1@gmail.com>
2018-11-04 15:30:45 +0100

    tak

---
+++ js/index.js
@@ -0,0 +1,22 @@
+
+
+			 
+			var NepDefCon = '1';
+			 
+			var NepPlayer = "#emu";
+			 
+			var NepEmu = "nds";
+			 
+			var NepZoom = "enable";
+			 
+			var NepMaxWidth = "100%";
+			 
+			var NepAutoStart = true;
+			 
+			var NepLang = "en";
+			 
+			var Emundsair = "false";
+			 
+			var gameUrl = "https://emulatoronline.comwp-content/uploads/2018/01/bomberman-2.zip";
+				 
+			var SaveTitle = "bomberman-2";
